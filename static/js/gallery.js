2a9b91c2cc8559a2d656a7bd3e23a96a698d95c5
mihael <bolsunovskiy@gmail.com>
2013-12-02 18:05:21 +0200

    fix gallery with fancybox, or lightbox fix contact form css fix comments form css

---
+++ static/js/gallery.js
@@ -0,0 +1,13 @@
+$(function() {
+    $("a[rel=thumbnail_group]").fancybox({
+				'transitionIn'		: 'none',
+				'transitionOut'		: 'none',
+				'titlePosition' 	: 'over',
+				'titleFormat'		: function(title, currentArray, currentIndex, currentOpts) {
+					return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+				}
+			});
+
+
+
+});
